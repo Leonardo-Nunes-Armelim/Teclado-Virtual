f7cdab738faefce3d3b579796130f5c913be6f1b
Leonardo Armelim <57201164+Leonardo-Nunes-Armelim@users.noreply.github.com>
2021-01-20 20:39:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,275 @@
+document.addEventListener("keydown", EventKey);
+
+function EventKey() {
+    let key = event.key
+
+    document.getElementById("info").innerHTML = "You enter the key: " + event.key
+
+    if (key == 1) {
+        document.getElementById("1").style.backgroundColor = "white";
+        document.getElementById("1").style.color = "black";
+    } else {
+        document.getElementById("1").style.backgroundColor = "black";
+        document.getElementById("1").style.color = "white";
+    }
+    if (key == 2) {
+        document.getElementById("2").style.backgroundColor = "white";
+        document.getElementById("2").style.color = "black";
+    } else {
+        document.getElementById("2").style.backgroundColor = "black";
+        document.getElementById("2").style.color = "white";
+    }
+    if (key == 3) {
+        document.getElementById("3").style.backgroundColor = "white";
+        document.getElementById("3").style.color = "black";
+    } else {
+        document.getElementById("3").style.backgroundColor = "black";
+        document.getElementById("3").style.color = "white";
+    }
+    if (key == 4) {
+        document.getElementById("4").style.backgroundColor = "white";
+        document.getElementById("4").style.color = "black";
+    } else {
+        document.getElementById("4").style.backgroundColor = "black";
+        document.getElementById("4").style.color = "white";
+    }
+    if (key == 5) {
+        document.getElementById("5").style.backgroundColor = "white";
+        document.getElementById("5").style.color = "black";
+    } else {
+        document.getElementById("5").style.backgroundColor = "black";
+        document.getElementById("5").style.color = "white";
+    }
+    if (key == 6) {
+        document.getElementById("6").style.backgroundColor = "white";
+        document.getElementById("6").style.color = "black";
+    } else {
+        document.getElementById("6").style.backgroundColor = "black";
+        document.getElementById("6").style.color = "white";
+    }
+    if (key == 7) {
+        document.getElementById("7").style.backgroundColor = "white";
+        document.getElementById("7").style.color = "black";
+    } else {
+        document.getElementById("7").style.backgroundColor = "black";
+        document.getElementById("7").style.color = "white";
+    }
+    if (key == 8) {
+        document.getElementById("8").style.backgroundColor = "white";
+        document.getElementById("8").style.color = "black";
+    } else {
+        document.getElementById("8").style.backgroundColor = "black";
+        document.getElementById("8").style.color = "white";
+    }
+    if (key == 9) {
+        document.getElementById("9").style.backgroundColor = "white";
+        document.getElementById("9").style.color = "black";
+    } else {
+        document.getElementById("9").style.backgroundColor = "black";
+        document.getElementById("9").style.color = "white";
+    }
+    if (key == 0) {
+        document.getElementById("0").style.backgroundColor = "white";
+        document.getElementById("0").style.color = "black";
+    } else {
+        document.getElementById("0").style.backgroundColor = "black";
+        document.getElementById("0").style.color = "white";
+    }
+    if (key == "q") {
+        document.getElementById("Q").style.backgroundColor = "white";
+        document.getElementById("Q").style.color = "black";
+    } else {
+        document.getElementById("Q").style.backgroundColor = "black";
+        document.getElementById("Q").style.color = "white";
+    }
+    if (key == "w") {
+        document.getElementById("W").style.backgroundColor = "white";
+        document.getElementById("W").style.color = "black";
+    } else {
+        document.getElementById("W").style.backgroundColor = "black";
+        document.getElementById("W").style.color = "white";
+    }
+    if (key == "e") {
+        document.getElementById("E").style.backgroundColor = "white";
+        document.getElementById("E").style.color = "black";
+    } else {
+        document.getElementById("E").style.backgroundColor = "black";
+        document.getElementById("E").style.color = "white";
+    }
+    if (key == "r") {
+        document.getElementById("R").style.backgroundColor = "white";
+        document.getElementById("R").style.color = "black";
+    } else {
+        document.getElementById("R").style.backgroundColor = "black";
+        document.getElementById("R").style.color = "white";
+    }
+    if (key == "t") {
+        document.getElementById("T").style.backgroundColor = "white";
+        document.getElementById("T").style.color = "black";
+    } else {
+        document.getElementById("T").style.backgroundColor = "black";
+        document.getElementById("T").style.color = "white";
+    }
+    if (key == "y") {
+        document.getElementById("Y").style.backgroundColor = "white";
+        document.getElementById("Y").style.color = "black";
+    } else {
+        document.getElementById("Y").style.backgroundColor = "black";
+        document.getElementById("Y").style.color = "white";
+    }
+    if (key == "u") {
+        document.getElementById("U").style.backgroundColor = "white";
+        document.getElementById("U").style.color = "black";
+    } else {
+        document.getElementById("U").style.backgroundColor = "black";
+        document.getElementById("U").style.color = "white";
+    }
+    if (key == "i") {
+        document.getElementById("I").style.backgroundColor = "white";
+        document.getElementById("I").style.color = "black";
+    } else {
+        document.getElementById("I").style.backgroundColor = "black";
+        document.getElementById("I").style.color = "white";
+    }
+    if (key == "o") {
+        document.getElementById("O").style.backgroundColor = "white";
+        document.getElementById("O").style.color = "black";
+    } else {
+        document.getElementById("O").style.backgroundColor = "black";
+        document.getElementById("O").style.color = "white";
+    }
+    if (key == "p") {
+        document.getElementById("P").style.backgroundColor = "white";
+        document.getElementById("P").style.color = "black";
+    } else {
+        document.getElementById("P").style.backgroundColor = "black";
+        document.getElementById("P").style.color = "white";
+    }
+    if (key == "a") {
+        document.getElementById("A").style.backgroundColor = "white";
+        document.getElementById("A").style.color = "black";
+    } else {
+        document.getElementById("A").style.backgroundColor = "black";
+        document.getElementById("A").style.color = "white";
+    }
+    if (key == "s") {
+        document.getElementById("S").style.backgroundColor = "white";
+        document.getElementById("S").style.color = "black";
+    } else {
+        document.getElementById("S").style.backgroundColor = "black";
+        document.getElementById("S").style.color = "white";
+    }
+    if (key == "d") {
+        document.getElementById("D").style.backgroundColor = "white";
+        document.getElementById("D").style.color = "black";
+    } else {
+        document.getElementById("D").style.backgroundColor = "black";
+        document.getElementById("D").style.color = "white";
+    }
+    if (key == "f") {
+        document.getElementById("F").style.backgroundColor = "white";
+        document.getElementById("F").style.color = "black";
+    } else {
+        document.getElementById("F").style.backgroundColor = "black";
+        document.getElementById("F").style.color = "white";
+    }
+    if (key == "g") {
+        document.getElementById("G").style.backgroundColor = "white";
+        document.getElementById("G").style.color = "black";
+    } else {
+        document.getElementById("G").style.backgroundColor = "black";
+        document.getElementById("G").style.color = "white";
+    }
+    if (key == "h") {
+        document.getElementById("H").style.backgroundColor = "white";
+        document.getElementById("H").style.color = "black";
+    } else {
+        document.getElementById("H").style.backgroundColor = "black";
+        document.getElementById("H").style.color = "white";
+    }
+    if (key == "j") {
+        document.getElementById("J").style.backgroundColor = "white";
+        document.getElementById("J").style.color = "black";
+    } else {
+        document.getElementById("J").style.backgroundColor = "black";
+        document.getElementById("J").style.color = "white";
+    }
+    if (key == "k") {
+        document.getElementById("K").style.backgroundColor = "white";
+        document.getElementById("K").style.color = "black";
+    } else {
+        document.getElementById("K").style.backgroundColor = "black";
+        document.getElementById("K").style.color = "white";
+    }
+    if (key == "l") {
+        document.getElementById("L").style.backgroundColor = "white";
+        document.getElementById("L").style.color = "black";
+    } else {
+        document.getElementById("L").style.backgroundColor = "black";
+        document.getElementById("L").style.color = "white";
+    }
+    if (key == "ç") {
+        document.getElementById("Ç").style.backgroundColor = "white";
+        document.getElementById("Ç").style.color = "black";
+    } else {
+        document.getElementById("Ç").style.backgroundColor = "black";
+        document.getElementById("Ç").style.color = "white";
+    }
+    if (key == "z") {
+        document.getElementById("Z").style.backgroundColor = "white";
+        document.getElementById("Z").style.color = "black";
+    } else {
+        document.getElementById("Z").style.backgroundColor = "black";
+        document.getElementById("Z").style.color = "white";
+    }
+    if (key == "x") {
+        document.getElementById("X").style.backgroundColor = "white";
+        document.getElementById("X").style.color = "black";
+    } else {
+        document.getElementById("X").style.backgroundColor = "black";
+        document.getElementById("X").style.color = "white";
+    }
+    if (key == "c") {
+        document.getElementById("C").style.backgroundColor = "white";
+        document.getElementById("C").style.color = "black";
+    } else {
+        document.getElementById("C").style.backgroundColor = "black";
+        document.getElementById("C").style.color = "white";
+    }
+    if (key == "v") {
+        document.getElementById("V").style.backgroundColor = "white";
+        document.getElementById("V").style.color = "black";
+    } else {
+        document.getElementById("V").style.backgroundColor = "black";
+        document.getElementById("V").style.color = "white";
+    }
+    if (key == "b") {
+        document.getElementById("B").style.backgroundColor = "white";
+        document.getElementById("B").style.color = "black";
+    } else {
+        document.getElementById("B").style.backgroundColor = "black";
+        document.getElementById("B").style.color = "white";
+    }
+    if (key == "n") {
+        document.getElementById("N").style.backgroundColor = "white";
+        document.getElementById("N").style.color = "black";
+    } else {
+        document.getElementById("N").style.backgroundColor = "black";
+        document.getElementById("N").style.color = "white";
+    }
+    if (key == "m") {
+        document.getElementById("M").style.backgroundColor = "white";
+        document.getElementById("M").style.color = "black";
+    } else {
+        document.getElementById("M").style.backgroundColor = "black";
+        document.getElementById("M").style.color = "white";
+    }
+    if (key == " ") {
+        document.getElementById("Space").style.backgroundColor = "white";
+        document.getElementById("Space").style.color = "black";
+    } else {
+        document.getElementById("Space").style.backgroundColor = "black";
+        document.getElementById("Space").style.color = "white";
+    }
+}
+
